e200747e9279899a24c4e2315986c8824c74ce78
alig917 <muhammadalinewforai@gmail.com>
2026-03-31 19:18:08 +0500

    Fix GitHub Pages path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/alpha-practice/" : "/",
+  base: mode === "production" ? "/alpha-practicee/" : "/",
   server: {
     host: "::",
     port: 8080,
